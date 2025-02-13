e3ccb33fa7ad6de4d83a2fd60a4a3a1d267a5aa7
Nafis <ahmednafis74@gmail.com>
2025-02-13 20:58:34 +0600

    added hero from banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,6 +7,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import Hero from "./Hero";
+
 const Banner = () => {
   return (
     <Swiper
@@ -24,62 +26,33 @@ const Banner = () => {
       className="bg-banner5 bg-cover bg-no-repeat bg-center min-h-screen grid place-content-center"
     >
       <SwiperSlide>
-        <div className="relative">
-          <img
-            src={
-              "https://i.ibb.co.com/bmR56v2/dec912b4-84e8-4c5f-9c57-81c639979844.webp"
-            }
-            className="w-full object-cover opacity-40 "
-            alt=""
-          />
-          <div className="absolute inset-0 grid place-content-center text-white/80 space-y-4 bg-black/40">
-            <h2 className="font-bold text-xl md:text-5xl text-center">
-              Got Questions? We&apos;ve Got Answers!
-            </h2>
-            <p className="font-semibold text-xl md:text-2xl text-center">
-              Ask anything, explore ideas, and get the insights you need.
-            </p>
-          </div>
-        </div>
+        <Hero
+          src={
+            "https://i.ibb.co.com/bmR56v2/dec912b4-84e8-4c5f-9c57-81c639979844.webp"
+          }
+          title={"Got Questions? We've Got Answers!"}
+          subtitle={
+            "Ask anything, explore ideas, and get the insights you need."
+          }
+        ></Hero>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="relative">
-          <img
-            src={
-              "https://i.ibb.co.com/qnntHPK/0420af1a-29fd-49a8-a90e-7be1ec83dca6.webp"
-            }
-            className="w-full  object-cover opacity-40"
-            alt=""
-          />
-
-          <div className="absolute inset-0 grid place-content-center text-white/80 space-y-4 bg-black/40">
-            <h2 className="font-bold text-xl md:text-5xl text-center">
-              Reliable Replies, Smart Recommendations
-            </h2>
-            <p className="font-semibold text-xl md:text-2xl text-center">
-              Get thoughtful suggestions from a community that cares.
-            </p>
-          </div>
-        </div>
+        <Hero
+          src={
+            "https://i.ibb.co.com/qnntHPK/0420af1a-29fd-49a8-a90e-7be1ec83dca6.webp"
+          }
+          title={"Reliable Replies, Smart Recommendations"}
+          subtitle={"Get thoughtful suggestions from a community that cares."}
+        ></Hero>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="relative">
-          <img
-            src={
-              "https://i.ibb.co.com/4YkdhPb/5e2f8743-c682-458c-b195-315ff623c780.webp"
-            }
-            className="w-full  object-cover opacity-40"
-            alt=""
-          />
-          <div className="absolute inset-0 grid place-content-center text-white/80 space-y-4 bg-black/40">
-            <h2 className="font-bold text-xl md:text-5xl text-center">
-              Authenticity You Can Trust
-            </h2>
-            <p className="font-semibold text-xl md:text-2xl text-center">
-              Verified insights and real recommendations, just for you.
-            </p>
-          </div>
-        </div>
+        <Hero
+          src={
+            "https://i.ibb.co.com/4YkdhPb/5e2f8743-c682-458c-b195-315ff623c780.webp"
+          }
+          title={"Authenticity You Can Trust"}
+          subtitle={"Verified insights and real recommendations, just for you."}
+        ></Hero>
       </SwiperSlide>
     </Swiper>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+const Hero = ({ src, title, subtitle }) => {
+  return (
+    <div className="relative">
+      <img src={src} className="w-full object-cover opacity-40 " alt="" />
+      <div
+        className="absolute inset-0 grid place-content-center text-white/80 
+      space-y-4 bg-black/30"
+      >
+        <h2 className="font-bold  md:text-3xl lg:text-5xl text-center">
+          {title}
+        </h2>
+        <p className="font-semibold  md:text-2xl text-center">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  src: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+};
+
+export default Hero;
