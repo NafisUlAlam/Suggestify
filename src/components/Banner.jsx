bda6db4cf26125791fdb45f7126d33b1a1d9cbf1
Nafis <ahmednafis74@gmail.com>
2025-02-13 20:41:01 +0600

    added text to banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -24,33 +24,62 @@ const Banner = () => {
       className="bg-banner5 bg-cover bg-no-repeat bg-center min-h-screen grid place-content-center"
     >
       <SwiperSlide>
-        <div>
+        <div className="relative">
           <img
             src={
               "https://i.ibb.co.com/bmR56v2/dec912b4-84e8-4c5f-9c57-81c639979844.webp"
             }
-            className="w-full object-cover opacity-60"
+            className="w-full object-cover opacity-40 "
             alt=""
           />
+          <div className="absolute inset-0 grid place-content-center text-white/80 space-y-4 bg-black/40">
+            <h2 className="font-bold text-xl md:text-5xl text-center">
+              Got Questions? We&apos;ve Got Answers!
+            </h2>
+            <p className="font-semibold text-xl md:text-2xl text-center">
+              Ask anything, explore ideas, and get the insights you need.
+            </p>
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <img
-          src={
-            "https://i.ibb.co.com/qnntHPK/0420af1a-29fd-49a8-a90e-7be1ec83dca6.webp"
-          }
-          className="w-full  object-cover opacity-60"
-          alt=""
-        />
+        <div className="relative">
+          <img
+            src={
+              "https://i.ibb.co.com/qnntHPK/0420af1a-29fd-49a8-a90e-7be1ec83dca6.webp"
+            }
+            className="w-full  object-cover opacity-40"
+            alt=""
+          />
+
+          <div className="absolute inset-0 grid place-content-center text-white/80 space-y-4 bg-black/40">
+            <h2 className="font-bold text-xl md:text-5xl text-center">
+              Reliable Replies, Smart Recommendations
+            </h2>
+            <p className="font-semibold text-xl md:text-2xl text-center">
+              Get thoughtful suggestions from a community that cares.
+            </p>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        <img
-          src={
-            "https://i.ibb.co.com/4YkdhPb/5e2f8743-c682-458c-b195-315ff623c780.webp"
-          }
-          className="w-full  object-cover opacity-60"
-          alt=""
-        />
+        <div className="relative">
+          <img
+            src={
+              "https://i.ibb.co.com/4YkdhPb/5e2f8743-c682-458c-b195-315ff623c780.webp"
+            }
+            className="w-full  object-cover opacity-40"
+            alt=""
+          />
+          <div className="absolute inset-0 grid place-content-center text-white/80 space-y-4 bg-black/40">
+            <h2 className="font-bold text-xl md:text-5xl text-center">
+              Authenticity You Can Trust
+            </h2>
+            <p className="font-semibold text-xl md:text-2xl text-center">
+              Verified insights and real recommendations, just for you.
+            </p>
+          </div>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
