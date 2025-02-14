0ba3114571cfecb4251abb4e90af460610482ab8
Nafis <ahmednafis74@gmail.com>
2025-02-14 19:48:36 +0600

    modal design changed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,11 +6,16 @@ const Modal = ({ isOpen, onClose }) => {
   return ReactDOM.createPortal(
     <>
       <div className="fixed bg-black/50 inset-0"></div>
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-text border-2 w-1/2 p-10 bg-background">
-        <button onClick={onClose} className="btn bg-primary border-0 text-text">
-          Close
-        </button>
-        <h2>Hello</h2>
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-text border-2 w-1/2 h-1/2 p-16 bg-background rounded-lg">
+        <div className="relative">
+          <button
+            onClick={onClose}
+            className="btn w-8 h-8 min-h-0 p-0 bg-primary rounded-full hover:bg-orange-400 border-0 text-text fixed -right-2 -top-2"
+          >
+            X
+          </button>
+        </div>
+        <h2 className="overflow-auto">Hello</h2>
       </div>
     </>,
     document.getElementById("portal")
